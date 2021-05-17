b16ea98a075cc7f6b8bd73ed5ed5ceb56b5f0dc6
cxk-53 <74444486+Sherlockcxk@users.noreply.github.com>
2021-05-16 19:35:47 -0700

    Add files via upload

---
+++ src/SingMethod.py
@@ -0,0 +1,17 @@
+from speech_recognition import Recognizer
+from wave import open as w_open
+
+rec = Recognizer()
+
+
+class RecordCode:
+    pass
+
+class RecognizeMP3:
+    pass
+
+class BagOfWords:
+    pass
+
+class GenerateCode:
+    pass
