fcd651d954b32858ed9ef6e616e97ed57fc1cfca
i use kali btw <74444486+Sherlockcxk@users.noreply.github.com>
2021-08-29 13:50:24 -0700

    Update RickRoll.py

--- src-py/RickRoll.py
+++ src-py/RickRoll.py
@@ -2,13 +2,8 @@ from time import time
 start = time()
 
 from sys import argv, stdout
-# from os.path import exists
-# from traceback2 import format_exc
+from argparse import ArgumentParser
 
-# from interpreter import run_in_interpreter
-# from pyrickroll import run_in_py, Token
-# from crickroll import run_in_cpp
-# import AudioGenerator
 
 # Help message
 rick_help = """
@@ -33,85 +28,54 @@ def play_audio(src_file_name):
         for statement in content:
             obj = Token(statement)
 
-            for i in range(len(obj.t_types)):
+            while len(obj.v_types) != 0:
                 AudioGenerator.play(obj.t_values[i])
 
 def main():
-    is_audio = False
-    is_help = False
-    show_time = False
 
-    is_cpp = False
-    is_py = False
-    is_intpr = False
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("file", nargs='?', default="")
+    arg_parser.add_argument("-py", action = "store_true")
+    arg_parser.add_argument("-cpp", action = "store_true")
+    arg_parser.add_argument("-intpr", action = "store_true")
+    arg_parser.add_argument("--time", action = "store_true")
+    arg_parser.add_argument("--audio", action = "store_true")
+    args = arg_parser.parse_args()
 
-    src_file_name = ''
+    src_file_name = args.file
 
-    if len(argv) <= 1:
-        exit(rick_help)
-
-    for i in range(len(argv)):
-        current_arg = argv[i].lower()
-
-        # Run code -py [file_name]
-        if current_arg == '-py':
-            src_file_name = argv[i + 1]
-            is_py = True
-        # Run code -cpp [file_name]
-        if current_arg == '-cpp':
-            src_file_name = argv[i + 1]
-            is_cpp = True
-        # Run code -intpr [file_name]
-        if current_arg == '-intpr':
-            src_file_name = argv[i + 1]
-            is_intpr = True
-        # Generate audio. --audio [Output audio file name]
-        if current_arg == '--audio':
-            is_audio = True
-        # Help message
-        if current_arg == '--help':
-            is_help = True
-
-        # Show execution time
-        if current_arg == '--time':
-            show_time = True
 
     # Run the RickRoll program
-    if src_file_name:
+    if args.file:
         from os.path import exists
-        if exists(src_file_name):
-            # Convert .rickroll to C++
-            if is_cpp:
-                from crickroll import run_in_cpp
-                run_in_cpp(src_file_name)
+        # Convert .rickroll to C++
+        if args.cpp:
+            from crickroll import run_in_cpp
+            run_in_cpp(args.file)
 
-            # Convert .rickroll to Python
-            elif is_py:
-                try:
-                    from pyrickroll import run_in_py
-                    exec(run_in_py(src_file_name), globals(), globals())
-                except:
-                    from traceback2 import format_exc
-                    error_msg = format_exc().split('File "<string>",')[-1]
-                    stdout.write(f'Exception in{error_msg}')
+        # Convert .rickroll to Python
+        elif args.py:
+            try:
+                from pyrickroll import run_in_py
+                exec(run_in_py(args.file), globals(), globals())
+            except:
+                from traceback2 import format_exc
+                error_msg = format_exc().split('File "<string>",')[-1]
+                stdout.write(f'Exception in{error_msg}')
 
-            # Execute .rickroll using the interpreter
-            elif is_intpr:
-                from interpreter import run_in_interpreter
-                run_in_interpreter(src_file_name)
+        # Execute .rickroll using the interpreter
+        elif args.intpr:
+            from interpreter import run_in_interpreter
+            run_in_interpreter(args.file)
 
-        else: exit(f"File [{src_file_name}] doesn't exist...")
     else: stdout.write('Warning: [Not executing any script...]')
 
 
-    if is_audio:
-        play_audio(src_file_name)
+    if args.audio:
+        play_audio(args.file)
 
-    if is_help:
-        stdout.write(rick_help)
-
-    if show_time:
-        stdout.write(f'\nExecution Time: [{time() - start}] sec.')
+    if args.time:
+        stdout.write(f'\nExecution Time: [{time() - start}] sec.\n')
 
 
 if __name__ == "__main__":
