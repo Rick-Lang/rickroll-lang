a7a10baeb3464629c0626b0847897260cb14083b
Kaqtus14 <kaqtus14@gmail.com>
2022-02-18 22:59:33 +0100

    Require file argument

--- src-py/RickRoll.py
+++ src-py/RickRoll.py
@@ -1,9 +1,9 @@
 from time import time
-start = time()      # Set and start a timer
-
 from sys import stdout
 from traceback import format_exc
 from argparse import ArgumentParser
+
+start = time()      # Set and start a timer
 
 
 def play_audio(src_file_name):
@@ -21,40 +21,34 @@ def play_audio(src_file_name):
             for i in range(len(tok.t_values)):
                 AudioGenerator.play(tok.t_values[i])
 
+
 def main():
 
     arg_parser = ArgumentParser()
-    arg_parser.add_argument("file", nargs='?', default="")
-    arg_parser.add_argument("-cpp", action = "store_true")
-    arg_parser.add_argument("-intpr", action = "store_true")
-    arg_parser.add_argument("--time", action = "store_true")
-    arg_parser.add_argument("--audio", action = "store_true")
+    arg_parser.add_argument("file")
+    arg_parser.add_argument("-cpp", action="store_true")
+    arg_parser.add_argument("-intpr", action="store_true")
+    arg_parser.add_argument("--time", action="store_true")
+    arg_parser.add_argument("--audio", action="store_true")
     args = arg_parser.parse_args()
 
-    # Run the RickRoll program
-    if args.file:
-        from os.path import exists
-        # Convert .rickroll to C++
-        if args.cpp:
-            from crickroll import run_in_cpp
-            run_in_cpp(args.file)
+    # Convert .rickroll to C++
+    if args.cpp:
+        from crickroll import run_in_cpp
+        run_in_cpp(args.file)
 
-        # Execute .rickroll using the interpreter
-        elif args.intpr:
-            from interpreter import run_in_interpreter
-            run_in_interpreter(args.file)
-
-        else:
-            try:
-                from pyrickroll import run_in_py
-                exec(run_in_py(args.file), globals(), globals())
-            except Exception:
-                error_msg = format_exc().split('File "<string>",')[-1]
-                stdout.write(f'Exception in{error_msg}')
+    # Execute .rickroll using the interpreter
+    elif args.intpr:
+        from interpreter import run_in_interpreter
+        run_in_interpreter(args.file)
 
     else:
-        stdout.write('Warning: [Not executing any script...]')
-
+        try:
+            from pyrickroll import run_in_py
+            exec(run_in_py(args.file), globals(), globals())
+        except Exception:
+            error_msg = format_exc().split('File "<string>",')[-1]
+            stdout.write(f'Exception in{error_msg}')
 
     if args.audio:
         play_audio(args.file)
